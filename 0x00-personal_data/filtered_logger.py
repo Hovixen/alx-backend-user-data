38071da3415e563a7e05137f7ab06cbaf9f34f83
Hovixen <orevaerijo@gmail.com>
2024-05-30 19:29:17 +0000

    a removal into .gitignore

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,11 +21,11 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ function formats the log message """
-        msg = super(RedactingFormatter, self).format(record=record)
+        msg = super(RedactingFormatter, self).format(record)
         return = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
