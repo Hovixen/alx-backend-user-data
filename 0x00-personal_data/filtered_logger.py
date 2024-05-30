b006eadd9dc74f7301840ceee4029c8f76ebde75
Hovixen <orevaerijo@gmail.com>
2024-05-30 18:10:12 +0000

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ from typing import List
 
 
 def filter_datum(fields: List, redaction: str,
-        message: str, separator: str) -> str:
+                 message: str, separator: str) -> str:
     """ function returns log message obfuscated """
     msg = re.sub("({})=[^{}]+".format("|".join(fields), separator),
                  lambda r: "{}={}".format(r.group(1), redaction), message)
