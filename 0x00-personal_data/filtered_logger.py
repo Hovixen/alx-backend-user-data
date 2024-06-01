660a8ab4a7befc70491539fb5bbd79a09031ae35
Hovixen <orevaerijo@gmail.com>
2024-06-01 20:55:11 +0000

    main

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -64,3 +64,23 @@ def get_db() -> connection.MySQLConnection:
             password=db_password,
             database=db_database
     )
+
+
+def main():
+    """
+    main entry point
+    """
+    db = get_db()
+    logger = get_logger()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = cursor.column_names
+    for row in cursor:
+        message = "".join("{}={}; ".format(k, v) for k, v in zip(fields, row))
+        logger.info(message.strip())
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
