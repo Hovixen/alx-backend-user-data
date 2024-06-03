a57fe2fab0fb634f7d7455ee496886fbe0eb364a
Hovixen <orevaerijo@gmail.com>
2024-06-03 20:49:02 +0000

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,20 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ function requires auth """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+
+        for x_path in excluded_paths:
+            if not x_path.endswith('/'):
+                x_path += '/'
+            if path == x_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ header authorization function """
