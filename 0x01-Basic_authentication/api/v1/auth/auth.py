e1cdd1af5f8b4b9f22f8ff445bcbcd5e0d40f0eb
Hovixen <orevaerijo@gmail.com>
2024-06-03 18:49:35 +0000

    tatask 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Auth Class """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ the auth class"""
+    def __init__(self):
+        """ initializing the class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ function requires auth """
+        return False, path, excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """ header authorization function """
+        return None, request
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user function """
+        return None, request
