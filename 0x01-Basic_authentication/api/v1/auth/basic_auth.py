2a348fd002a7ae21b846dc17b923f02190ebbae3
Hovixen <orevaerijo@gmail.com>
2024-06-04 18:27:32 +0000

    task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,3 +60,30 @@ class BasicAuth(Auth):
                 if user.is_valid_password(user_pwd):
                     return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ retrieves the User instance of a request """
+        if request is None:
+            return None
+
+        header = self.authorization_header(request)
+        if header is None:
+            return None
+
+        extract = self.extract_base64_authorization_header(header)
+        if extract is None:
+            return None
+
+        decode = self.decode_base64_authorization_header(extract)
+        if decode is None:
+            return None
+
+        email, pwd = self.extract_user_credentials(decode)
+        if email is None or pwd is None:
+            return None
+
+        user = self.user_object_from_credentials(email, pwd)
+        if user is None:
+            return None
+
+        return user
