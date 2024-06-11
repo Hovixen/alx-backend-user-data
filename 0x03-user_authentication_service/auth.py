d37c0336e02816260fe0ed524e9acb68cdf8638c
Hovixen <orevaerijo@gmail.com>
2024-06-11 13:35:50 +0000

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,7 +4,7 @@ import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
-        """ returns the hased password in bytes """
-        salt = bcrypt.gensalt()
-        pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return pwd
+    """ returns the hased password in bytes """
+    salt = bcrypt.gensalt()
+    pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return pwd
