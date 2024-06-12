a7e38cbb94f2de77411b67888942279d6f7d50cc
Hovixen <orevaerijo@gmail.com>
2024-06-12 14:39:17 +0000

    task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,7 +47,7 @@ def logout():
     """ logs out user by deleting user session ID """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
+    if user is not None:
         AUTH.destroy_session(user.id)
         return redirect(url_for('index'))
     else:
