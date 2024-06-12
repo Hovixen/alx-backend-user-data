7215a3ac3448b423c8dff3b397c2f44d7b98f805
Hovixen <orevaerijo@gmail.com>
2024-06-12 16:20:58 +0000

    task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,7 +87,7 @@ def update_password():
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({'email': email, 'message': 'Password updated'})
-    execpt ValueError:
+    except ValueError:
         abort(403)
 
 
